0ca0a5673d5d45c2bfd3b37bf82f98cbec9d5076
Ossi Salmi <osalmi@iki.fi>
2016-07-10 02:57:29 +0300

    Use defaults to disable Spotlight suggestions

--- files/Library/Custom/postinstall.sh
+++ files/Library/Custom/postinstall.sh
@@ -123,6 +123,30 @@ set_user_defaults() {
     # Disable Spotlight suggestions
     defaults write "$USERPREFS/com.apple.Safari" UniversalSearchEnabled -bool false
     defaults write "$USERPREFS/com.apple.lookup" lookupEnabled -dict suggestionsEnabled 0
+    defaults write "$USERPREFS/com.apple.Spotlight" version -int 7
+    defaults write "$USERPREFS/com.apple.Spotlight" orderedItems '(
+        { enabled = 1; name = APPLICATIONS; },
+        { enabled = 0; name = "MENU_SPOTLIGHT_SUGGESTIONS"; },
+        { enabled = 1; name = "MENU_CONVERSION"; },
+        { enabled = 1; name = "MENU_EXPRESSION"; },
+        { enabled = 1; name = "MENU_DEFINITION"; },
+        { enabled = 1; name = "SYSTEM_PREFS"; },
+        { enabled = 1; name = DOCUMENTS; },
+        { enabled = 1; name = DIRECTORIES; },
+        { enabled = 1; name = PRESENTATIONS; },
+        { enabled = 1; name = SPREADSHEETS; },
+        { enabled = 1; name = PDF; },
+        { enabled = 1; name = MESSAGES; },
+        { enabled = 1; name = CONTACT; },
+        { enabled = 1; name = "EVENT_TODO"; },
+        { enabled = 1; name = IMAGES; },
+        { enabled = 1; name = BOOKMARKS; },
+        { enabled = 1; name = MUSIC; },
+        { enabled = 1; name = MOVIES; },
+        { enabled = 1; name = FONTS; },
+        { enabled = 1; name = "MENU_OTHER"; },
+        { enabled = 0; name = "MENU_WEBSEARCH"; }
+    )'
     # Disable OCSP
     defaults write "$USERPREFS/com.apple.security.revocation" OCSPStyle -string "None"
     # Disable handover by default
